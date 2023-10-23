d8b4fd090888ec9e00edb1f939e417bcb1b4de0f
mouradbel <97679229+mouradbel@users.noreply.github.com>
2023-10-23 18:13:07 +0200

    Update Paquet.java
    Début classe Paquet

--- src/Paquet.java
+++ src/Paquet.java
@@ -7,18 +7,18 @@ public class Paquet {
 
     private ArrayList<Carte> cartes;
 
-    //Fonction tirer cartes pour pouvoir enlever du tas une carte et la retourner
+    //Fonction pour tirer cartes pour pouvoir enlever du tas une carte et la retourner
     Carte TirerCarte(){
     	
     	Carte carteRetiree;
     	
-    	//nb alea taille paquet
-    	//ens stocker la carte l'enlever du paquet et la return de la fonction
+    	//nombre aléatoire de la taille du paquet
+    	//ensuite stocker la carte l'enlever du paquet et la return de la fonction
     	Random random = new Random();
-		int carteAleatoire = random.nextInt(cartes.size());
-		carteRetiree = cartes.get(carteAleatoire);
-		cartes.remove(carteAleatoire);
-		return carteRetiree;
+	int carteAleatoire = random.nextInt(cartes.size());
+	carteRetiree = cartes.get(carteAleatoire);
+	cartes.remove(carteAleatoire);
+	return carteRetiree;
     }
     
     
