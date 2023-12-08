7ac065b50fff0762e09acd91beb25dbd4e0c3040
mouradbel <97679229+mouradbel@users.noreply.github.com>
2023-12-08 11:43:22 +0100

    oublié les privates

--- src/main/java/CORRegles/MemeValeurEtMemeCouleurHandler.java
+++ src/main/java/CORRegles/MemeValeurEtMemeCouleurHandler.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 
 public class MemeValeurEtMemeCouleurHandler extends ExpertCOR {
 
-    Joueur joueur;
-    Jeu jeu;
+    private Joueur joueur;
+    private Jeu jeu;
     public MemeValeurEtMemeCouleurHandler(Jeu jeu) {
         this.joueur = jeu.getJoueur();
         this.jeu = jeu;
